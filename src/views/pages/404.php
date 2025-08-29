d517146df2154297942f46da6ee6b864de817407
Dyyno <lilinhodantas45@gmail.com>
2025-08-28 21:52:19 -0300

    adicionado a pagina de 404 feita pelo figueiredo

--- src/views/pages/404.php
+++ src/views/pages/404.php
@@ -1,1 +1,32 @@
-<h1>Página não encontrada</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link
+      rel="shortcut icon"
+      href="<?=$base;?>/assets/images/logo_padrao.svg"
+      type="image/x-icon"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina não encontrada</title>
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/404.css">
+</head>
+<body>
+    <main>
+       <section class="container">
+    <img src="<?=$base;?>/assets/images/404foto.png" alt="Imagem 404" class="img-esquerda">
+    <div class="conteudo">
+        <h1><strong>4<b id="zero">0</b>4</strong></h1>
+        <p id="texto1">Essa página está em <b>obras</b></p>
+        <p id="subtexto">Volte depois ou explore outra rua do <b>Quibble</b></p>
+        <a href="<?=$base;?>/">
+            <button>Voltar para o feed</button>
+        </a>
+    </div>
+</section>
+
+
+    </main>
+    
+</body>
+</html>
