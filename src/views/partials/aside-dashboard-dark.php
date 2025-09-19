c174646081e479c25e0d7fcd02663612b2e70ad6
Gabriel Souza <gabrielpega6788@gmail.com>
2025-09-19 17:44:07 -0300

    Partial do sidebar do dashboard dark de secretário feito

---
+++ src/views/partials/aside-dashboard-dark.php
@@ -0,0 +1,90 @@
+<aside>
+        <h1>Quibble</h1>
+        <div class="aside-content">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="img/visao_geral_dashboard.svg" alt="">
+                            Visão Geral
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/reclamacoes_dashboard.svg" alt="">
+                            Reclamações
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/mapa_dashboard.svg" alt="">
+                            Mapa
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/comunidades_dashboard.svg" alt="">
+                            Comunidades
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/relatorios_dashboard.svg" alt="">
+                            Relatórios
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/gestao_interna_dashboard.svg" alt="">
+                            Gestão Interna
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/comunicacao_dashboard.svg" alt="">
+                            Comunicação
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/transparencia_dashboard.svg" alt="">
+                            Transparência
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/financeiro_dashboard.svg" alt="">
+                            Financeiro
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div id="div-btn">
+                <a href="">
+                    <img src="img/tema_dashboard.svg" alt="">
+                    Customisar
+                </a>
+            </div>
+            <hr>
+            <div class="config">
+                <div class="links">
+                    <div>
+                        <a href="">
+                            <img src="img/config_dashboard.svg" alt="">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img src="img/raio_dashboard.svg" alt="">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="">
+                            <img src="img/idioma_dashboard.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </aside>
